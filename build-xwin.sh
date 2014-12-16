f41c04f32ecb2e4625f181cac0d9698985711908
Gary Niu <gary.niu@gmail.com>
2014-12-16 15:24:43 +0800

    add oel6u6

--- build-xwin.sh
+++ build-xwin.sh
@@ -2,10 +2,10 @@
  
 set -e
 #export PATH=~/0.6.0_darwin_amd64:$PATH 
-rm build/OELW6_5.box || true
-packer build oracle-linux-6.5-xwin-x86_64.json
-vagrant box remove OELW6_5 || true
-vagrant box add OELW6_5 build/OELW6_5.box
+rm build/OELW6_6.box || true
+packer build oracle-linux-6.6-xwin-x86_64.json
+vagrant box remove OELW66 || true
+vagrant box add OELW66 build/OELW6_6.box
 
 #rm build/OELW7_0.box || true
 #packer build oracle-linux-7.0-xwin-x86_64.json
--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
  
 set -e
 
-export PATH=~/0.6.0_darwin_amd64:$PATH 
-rm build/OEL6_5.box || true
-packer build oracle-linux-6.5-x86_64.json
-vagrant box remove OEL6_5 || true
-vagrant box add OEL6_5 build/OEL6_5.box
+#export PATH=~/0.6.0_darwin_amd64:$PATH 
+rm build/OEL6_6.box || true
+packer build oracle-linux-6.6-x86_64.json
+vagrant box remove OEL66 || true
+vagrant box add OEL66 build/OEL6_6.box
